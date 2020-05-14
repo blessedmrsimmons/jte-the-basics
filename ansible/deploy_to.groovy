0c944bb8339805d3dab6e0210455c8472c5bf482
user <user@this.com>
2020-05-14 09:58:13 -0400

    Added Ansible.

---
+++ ansible/deploy_to.groovy
@@ -0,0 +1,6 @@
+void call(app_env){
+   stage("Deploy To: ${app_env.long_name}"){
+      println "Performing a deployment through ansible.."
+      app_env.ip_addresses.each{ ip-> println "deploying to ${ip}"}
+  }
+}
